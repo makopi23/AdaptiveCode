390c2599445b41e1b579515f6ba92b2c8ca69f27
makopi23 <kipersonalpc@gmail.com>
2017-01-15 13:33:25 +0900

    chapter02とchapter03のプロジェクトを追加

---
+++ Chapter3/ch3-null-object-pattern/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch3_null_object_pattern
+{
+    interface IUserRepository
+    {
+        IUser GetByID(Guid userID);
+    }
+
+    public class UserRepository : IUserRepository
+    {
+        //private string userId;
+        private ICollection<User> users;
+
+        public UserRepository()
+        {
+            users = new List<User>
+            {
+                new User(Guid.NewGuid()),
+                new User(Guid.NewGuid()),
+                new User(Guid.NewGuid()),
+                new User(Guid.NewGuid())
+            };
+        }
+
+        public IUser GetByID(Guid userID)
+        {
+            //return new User();
+            IUser userFound = users.SingleOrDefault(user => user.ID == userID);
+            if(userFound == null)
+            {
+                userFound = new NullUser();
+            }
+            return userFound;
+        }
+    }
+}
